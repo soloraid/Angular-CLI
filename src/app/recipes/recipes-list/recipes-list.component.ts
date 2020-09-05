64adaa5427afa0340d2b748493e8ab69c06b4365
soloraid <hm.praw@yahoo.com>
2020-09-05 22:57:07 +0430

    update app-components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+   recipes: Recipe[] = [
+     new Recipe('A Test Recipe', 'This is simple Test',
+       'https://images.immediate.co.uk/production/volatile/sites/2/2016/02/20501.jpg?webp=true&quality=90&crop=1px%2C328px%2C597px%2C257px&resize=597%2C253'),
+     new Recipe('A Test Recipe', 'This is simple Test',
+       'https://images.immediate.co.uk/production/volatile/sites/2/2016/02/20501.jpg?webp=true&quality=90&crop=1px%2C328px%2C597px%2C257px&resize=597%2C253')
+   ];
   constructor() { }
 
   ngOnInit(): void {
