4d502b584afa2f8d6f941d5a75fb4c9ce03e7742
hashem <hm.praw@yahoo.com>
2020-11-01 13:04:10 +0330

    bind Recipes-details

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -10,12 +10,17 @@ export class RecipesListComponent implements OnInit {
    recipes: Recipe[] = [
      new Recipe('A Test Recipe', 'This is simple Test',
        'https://images.immediate.co.uk/production/volatile/sites/2/2016/02/20501.jpg?webp=true&quality=90&crop=1px%2C328px%2C597px%2C257px&resize=597%2C253'),
-     new Recipe('A Test Recipe', 'This is simple Test',
+     new Recipe('Another Test Recipe', 'This is simple Test',
        'https://images.immediate.co.uk/production/volatile/sites/2/2016/02/20501.jpg?webp=true&quality=90&crop=1px%2C328px%2C597px%2C257px&resize=597%2C253')
    ];
+   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
